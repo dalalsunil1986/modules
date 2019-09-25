ce9c9c593f7b1c2680d64f4f9f68b73531d99e07
Rafal Mitula (PGS Software) <rmitula@pgs-soft.com>
2019-09-25 09:18:23 +0200

    source update and add gl

---
+++ datapipeline/data-providers/gl/modules/example-module/main.tf
@@ -0,0 +1,9 @@
+resource "aws_s3_bucket" "example" {
+  bucket = "rmitula-${var.env}"
+  acl    = "private"
+
+  tags = {
+    name  = "Terragrunt testing sample bucket"
+    env   = var.env
+  }
+}
